1a4b38f27d82cb6b65b4240a51d23874eca294de
alpha0021 <rushdi_123@hotmail.com>
2018-05-31 00:13:40 +0200

    new project folder added

---
+++ server.php
@@ -0,0 +1,50 @@
+<?php 
+	session_start();
+	//intializing variables
+	$name = "";
+	$address= "";
+	$id=0;
+	$edit_state = false;
+
+	//connection
+	$db= mysqli_connect('localhost','root','','crud')// database name
+
+	//if save button is clicked
+
+	if (isset($_POST['save'])) {
+		$name = $_POST['name'];
+		$address = $_POST['address'];
+
+		$query = "INSERT info(name,address) VALUES('$name','$address')";
+		mysqli_query($db, $query);
+		$_SESSION['msg'] = "Address saved";
+		header('location:index.php');//ridercting  homepage after inserting
+	}
+
+	//update records
+if (isset($_POST['update'])) {
+		$name = mysql_escape_string($_POST['name']);
+		$address = mysql_escape_string($_POST['address']);
+		$id= mysql_escape_string($_POST['id']);	
+
+		mysqli_query($db , "UPDATE info SET name='$name' , address='$address' WHERE id='$id'");	
+		$_SESSION['msg'] = "Address updated";
+		header('location:index.php');
+	}
+
+
+	//delete records
+
+	if (isset($_GET['del'])) {
+		$id=$_GET['del'];
+		mysqli_query($db, "DELETE FROM info WHERE id=$id");
+		$_SESSION['msg'] = "Address deleted";
+		header('location:index.php');
+	}
+
+
+
+	//retrieve records
+
+	$results = mysqli_query($db,"SELECT FROM info");
+ ?>
